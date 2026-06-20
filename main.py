dafa61f96ec8a0f1df41ee631e2ca96e394c852c
hadi arbabi <hadimojak@gmail.com>
2026-06-20 14:17:41 +0330

    Ignore env file

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+API_URL = "https://router.huggingface.co/hf-inference/models/openai/whisper-large-v3-turbo"
 
-@app.get("/")
-def read_root():
-    return {"message": "hi theerreee"}
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {HF_TOKEN}"
+}
 
 
-@app.get("/ask")
-def ask_ai(question: str):
-    return {"question": question, "answer": "this is mock"}
+@app.post("/transcribe")
+async def transcribe(file: UploadFile = File(...)):
+
+    if HF_TOKEN is None:
+        raise HTTPException(status_code=500, detail="HF_TOKEN not set")
+
+    audio_bytes = await file.read()
+
+    response = requests.post(
+        API_URL,
+        headers={
+            "Authorization": f"Bearer {HF_TOKEN}",
+            "Content-Type": file.content_type or "application/octet-stream"
+        },
+        data=audio_bytes,
+        timeout=120
+    )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail=response.text)
+
+    result = response.json()
+
+    return result
